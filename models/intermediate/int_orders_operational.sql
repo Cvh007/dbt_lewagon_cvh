6a18c0eb65af18c3824e756d58cc47d0c466a472
charles.vanhaelst@gmail.com <charles.vanhaelst@gmail.com>
2024-04-09 14:34:13 +0000

    finance days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,6 +2,10 @@ select
 orders_id
 , date_date
 , Round (SUM((margin + shipping_fee - logcost - ship_cost)),2) as operational_margin
+, ROUND (Sum (revenue),2) as revenue
+, ROUND (Sum (quantity),2) as quantity
+, ROUND (Sum (purchase_cost),2) as purchase_cost
+, ROUND (Sum (margin),2) as margin
 FROM 
 {{ref("int_orders_margin")}}
 Join {{ref("stg_raw__ship")}}
