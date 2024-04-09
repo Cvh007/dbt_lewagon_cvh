ba6b2d11efea3445dc8f3a37913d837840ec82af
charles.vanhaelst@gmail.com <charles.vanhaelst@gmail.com>
2024-04-09 14:16:13 +0000

    opertianol margin commit final

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,9 +1,9 @@
 select 
 orders_id
 , date_date
-, Round (SUM((margin + shipping_fee) - (logcost - ship_cost)),2) as operational_margin
+, Round (SUM((margin + shipping_fee - logcost - ship_cost)),2) as operational_margin
 FROM 
-{{ref("int_sales_margin")}}
+{{ref("int_orders_margin")}}
 Join {{ref("stg_raw__ship")}}
 Using (orders_id)
 Group by orders_id, date_date
